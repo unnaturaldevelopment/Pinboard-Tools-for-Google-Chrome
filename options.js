27bdf109c36ef06839da5604fec899c1f3ce6337
Decklin Foster <decklin@red-bean.com>
2010-08-19 10:25:35 -0400

    Add options page, option to open in current or new tab

---
+++ options.js
@@ -0,0 +1,22 @@
+function each_input(name, fn) {
+    var inputs = document.getElementsByTagName('input');
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].name === name)
+            fn.apply(inputs[i]);
+    }
+}
+
+function loadOptions() {
+    if ('open_in' in localStorage) {
+        each_input('open_in', function() {
+            this.checked = (this.value === localStorage['open_in']);
+        });
+    }
+}
+
+function saveOptions() {
+    each_input('open_in', function() {
+        if (this.checked)
+            localStorage['open_in'] = this.value;
+    });
+}
