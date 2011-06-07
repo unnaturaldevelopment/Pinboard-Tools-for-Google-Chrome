fb24a991db547139d06a5caa29d393fdb991eca7
Pinboard <support@pinboard.in>
2011-06-07 14:13:51 -0400

    Add source from the official pinboard extension
    The source in this commit is directly from the official Chrome
    extension published on June 7th 2011
    
    (cc: @pinboard, @mceglowski)

--- addPinboard.js
+++ addPinboard.js
@@ -1,12 +1,22 @@
-var pinboardUrl = "http://pinboard.in/add?";
+var BASE_URL = 'https://pinboard.in';
+var pinboardUrl = BASE_URL+"/add?";
 var url;
 var title;
-
 function add_bookmark() {
 	chrome.tabs.getSelected( null , function(tab) {
  		url = (tab.url);
  		title = (tab.title);
- 		window.open(pinboardUrl + 'url=' + url + '&title=' + title +' ','Pinboard','toolbar=no,width=700,height=350');
+		docurl = pinboardUrl + 'url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(title) + '&description=' + encodeURIComponent(pbdescription) + ' ','Pinboard','toolbar=no,width=700,height=350';
+		//Load save bookmark window in iFrame
+		pbiframe = document.createElement('iframe');
+		pbiframe.src = docurl;
+		pbiframe.frameBorder = "0";
+		pbiframe.width = "100%";
+		pbiframe.height = "100%";
+		document.body.innerHTML = "" ; //clear popup window
+		document.body.style.width = "700px";
+		document.body.style.height = "350px";//grow window
+		document.body.appendChild(pbiframe);
 	});
 }
 
@@ -14,12 +24,77 @@ function add_later() {
 	chrome.tabs.getSelected( null , function(tab) {
  		url = (tab.url);
  		title = (tab.title);
- 		window.open(pinboardUrl + 'later=yes&noui=yes&jump=close&url=' + url + '&title=' + title +' ','Pinboard','toolbar=no,width=100,height=100');
+ 		window.open(pinboardUrl + 'later=yes&noui=yes&jump=close&url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(title) + ' ','Pinboard','toolbar=no,width=100,height=100');
 	});
 }
 
-//sample popup URL from Pinboard:
-//javascript:q=location.href;if(document.getSelection){d=document.getSelection();}else{d='';};p=document.title;void(open('http://pinboard.in/add?url='+encodeURIComponent(q)+'&description='+encodeURIComponent(d)+'&title='+encodeURIComponent(p),'Pinboard',%20'toolbar=no,width=700,height=350'));
+//Below based on Pinboard.In Safari extension
+//https://pinboard.in/resources/safari/save_tabs/
+var SUBMIT_URL  = BASE_URL + '/tabs/save/';
+var DISPLAY_URL = BASE_URL + '/tabs/show/';
 
-//sample read later URL from Pinboard:
-//javascript:q=location.href;p=document.title;void(t=open('http://pinboard.in/add?later=yes&noui=yes&jump=close&url='+encodeURIComponent(q)+'&title='+encodeURIComponent(p),'Pinboard','toolbar=no,width=100,height=100'));t.blur();
+function save_tabs() {
+	chrome.windows.getAll({"populate" : true}, makeTabList);
+}
+
+/*
+// Not doing this yet, but could do with removing any chrome://, about:, etc tabs
+// check if there are any nonempty tabs open
+function hasContentTabs()
+{
+    var winz = safari.application.browserWindows;
+    
+    for (var i = 0; i < winz.length; i++)
+    {
+        var tabz = winz[i].tabs;
+        for (var j = 0; j < tabz.length; j++)
+        {
+            if (tabz[j].url)
+            {
+                return true;
+            }
+        }
+    }   
+    return false;
+}*/
+
+//Callback thingyamabob based on Chromium Developer Examples
+//http://src.chromium.org/viewvc/chrome/trunk/src/chrome/common/extensions/docs/examples/api/windows/merge_windows/background.html?revision=39116&view=markup
+
+function makeTabList(windows) {
+	winz = [];
+	result = { browser: "chrome",  windows: winz }; 
+
+	var chromeWinz = windows;
+
+	for (var i = 0; i < chromeWinz.length; i++) {
+		var chromeTabz = chromeWinz[i].tabs;
+		var tabz = [];
+
+		for (var j = 0; j < chromeTabz.length; j++) {
+			var cTab = chromeTabz[j];
+			if (cTab.url) {
+				tabz.push({	title:   cTab.title, 
+											url:     cTab.url }); 
+			}
+		}
+		winz.push(tabz);
+	}   
+	console.log(result);
+	tabList = result;
+
+	var params = new FormData();
+	var req   = new XMLHttpRequest(); 
+
+	params.append("data", JSON.stringify(result));
+	console.log(JSON.stringify(result));
+	req.open("POST", SUBMIT_URL, true);        
+
+	req.onreadystatechange = function() {
+	  if (req.readyState == 4) {
+			chrome.tabs.create({url:DISPLAY_URL});
+		}
+  }   
+	req.send(params);
+}
+
